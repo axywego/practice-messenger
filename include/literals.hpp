8643fc67f4c918a6ff79ba5e5c006c9f602ea614
axywego <pukeotim@gmail.com>
2026-06-19 14:16:07 +0300

    fix literals

--- include/literals.hpp
+++ include/literals.hpp
@@ -2,6 +2,10 @@
 
 #include <cstdint>
 
-constexpr unsigned long long operator""_MB(unsigned long long num) {
+constexpr unsigned long long operator""_KB(unsigned long long num) {
     return num * 1024;
+}
+
+constexpr unsigned long long operator""_MB(unsigned long long num) {
+    return num * 1024 * 1024;
 }
